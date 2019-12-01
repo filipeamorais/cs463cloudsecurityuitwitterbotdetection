11e3500d16a4928e928c888038b6594a799a25a5
filipeam <afilipem@gmail.com>
2019-11-30 18:19:11 -0600

    deleted: removed the unused comments.

--- deployment/run_keras_server.py
+++ deployment/run_keras_server.py
@@ -13,15 +13,7 @@ class ReusableForm(Form):
     # Starting seed
     seed = TextField("Enter twitter id :", validators=[
                      validators.InputRequired()])
-    # Diversity of predictions
-    '''diversity = DecimalField('Enter diversity:', default=0.8,
-                             validators=[validators.InputRequired(),
-                                         validators.NumberRange(min=0.5, max=5.0,
-                                                                message='Diversity must be between 0.5 and 5.')])'''
-    # Number of words
-    '''words = IntegerField('Enter number of words to generate:',
-                         default=50, validators=[validators.InputRequired(),
-                                                 validators.NumberRange(min=10, max=100, message='Number of words must be between 10 and 100')])'''
+    
     # Submit button
     submit = SubmitField("Enter")
 
